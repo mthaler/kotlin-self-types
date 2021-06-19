3baa55238b54bbd1f247afb5fc26f2aeb6010edf
Michael Thaler <michael.thaler@dlr.de>
2021-06-19 10:29:58 +0200

    Added Rect class.

--- src/main/kotlin/com/mthaler/selftypes/Shape.kt
+++ src/main/kotlin/com/mthaler/selftypes/Shape.kt
@@ -5,4 +5,12 @@ interface Shape {
     val position: Point
 }
 
-data class Circle(override val position: Point, val radius: Double): Shape
+data class Circle(override val position: Point, val radius: Double): Shape {
+
+    fun withRadius(radius: Double): Circle = copy(radius = radius)
+}
+
+data class Rect(override val position: Point, val size: Size): Shape {
+
+    fun withSize(size: Size): Rect = copy(size = size)
+}
